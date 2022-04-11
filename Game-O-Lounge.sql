e0fb38066ed369f51d8a5077007a713030ac5b45
Uthpala Hewagamage <102724809+Upzey@users.noreply.github.com>
2022-04-11 23:43:12 +0530

    Add files via upload

---
+++ Game-O-Lounge.sql
@@ -0,0 +1,20 @@
+--creation of the database
+CREATE DATABASE game_o_lounge;
+
+--creation of the table sign_up where the user details are stored and updated.
+CREATE TABLE sign_up (
+	Username VARCHAR(50) PRIMARY KEY,
+	Email VARCHAR(50),
+	Password VARCHAR(20),
+	C_Password VARCHAR(20),
+	Token INT(6),
+	Profile_Pic VARCHAR(50)
+);
+
+--creation of the table score where all the scores of the players are stored and updated.
+CREATE TABLE score (
+Username VARCHAR(40),
+Game VARCHAR(20),
+Score INT(2)
+CONSTRAINT pk_score PRIMARY KEY (Username,Game)
+);
